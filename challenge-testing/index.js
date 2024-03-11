bcd9a47ddf8bcc87ebeec55b88f39b3a43b11c91
Leonel Nusdeo <leonel.nusdeo@gmail.com>
2024-03-10 23:13:09 -0300

    Homework 05. Initial setup.

---
+++ challenge-testing/index.js
@@ -0,0 +1,5 @@
+class CarritoCompra {
+
+}
+
+module.exports = CarritoCompra;
---
+++ challenge-testing/tests/CarritoCompra.test.js
@@ -0,0 +1,14 @@
+const CarritoCompra = require("../index");
+
+describe("Grupo de tests de CarritoCompra"), () => {
+
+// constructor(): Inicializa el carrito como un array vacío.
+
+// agregarProducto(producto): Recibe un objeto representando un producto y lo agrega al carrito.
+
+// calcularTotal(): Calcula el total de la compra sumando los precios de todos los productos en el carrito.
+
+// aplicarDescuento(porcentaje): Aplica un descuento al total de la compra según el porcentaje especificado.	
+
+}
+

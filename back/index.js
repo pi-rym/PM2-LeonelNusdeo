9aa130529f3d59c056ef98b4923a3c6cbd76d189
Leonel Nusdeo <leonel.nusdeo@gmail.com>
2024-03-18 22:32:51 -0300

    Revert "Homework 11. Homework 09 & Homework 10 were not required to commit."
    This reverts commit f0bc6050001e52e9a4a3faaf0d213fcca55f3202.

--- back/index.js
+++ back/index.js
@@ -1,9 +1,3 @@
 const app = require('./src/server');
-const dbCon = require('./src/config/dbCon');
 
-dbCon()
-.then( () => {
-	app.listen(3000, () => console.log("Server on port 3000"));
-})
-.catch((err) => console.log("Database connection problem", err.message));
-
+app.listen(3000, () => console.log("server on port 3000"));

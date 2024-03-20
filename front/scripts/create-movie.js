a65e36ac023326ec1f14cd841a3ae76db912cc64
Leonel Nusdeo <leonel.nusdeo@gmail.com>
2024-03-19 22:42:52 -0300

    Homework 12. Actividad 01: Complete.

--- front/scripts/create-movie.js
+++ front/scripts/create-movie.js
@@ -21,4 +21,34 @@ function clearForm() {
 
 }
 
+const setMovieHandler = () => {
+    // Seleccionar los inputs de title, description e imgUrl.
+    const titleInput = document.getElementById("titleInput");
+    const yearInput = document.getElementById("yearInput");
+    const directorInput = document.getElementById("directorInput");
+    const durationInput = document.getElementById("durationInput");
+    const genreInput = document.getElementById("genreInput");
+    const rateInput = document.getElementById("rateInput");
+    const posterInput = document.getElementById("posterInput");
+    // Tomar los valores ingresados en los inputs y guardarlos en variables.
+    const title = titleInput.value;
+    const year = parseInt(yearInput.value);
+    const director = directorInput.value;
+    const duration = durationInput.value;
+    const genre = genreInput.value.split(', ');
+    const rate = parseFloat(rateInput.value);
+    const poster = posterInput.value;
+    // Validar que estos valores estén completos.
+    if(!title || !year || !director || !duration || !genre || !rate || !poster) {
+        return alert("Datos incompletos. Por favor ingrese todos los campos.");
+    } else {
+        const movie = { title, year, director, duration, genre, rate, poster };
+        console.log(movie);
+        return alert("Pelicula creada correctamente.");
+    }
+}
+
+
 validateForm();
+const setMovieButton = document.getElementById("addMovieButton");
+setMovieButton.addEventListener("click", setMovieHandler);

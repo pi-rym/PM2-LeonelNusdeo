dfd07d5f2cbc178312fdac85b545cd41f4368159
Leonel Nusdeo <leonel.nusdeo@gmail.com>
2024-03-20 03:25:33 -0300

    Homework 13. Actividad 02: Extra credit.

---
+++ back/src/middlewares/validateMovie.js
@@ -0,0 +1,10 @@
+const validateMovie = (req, res, next) => {
+    const { title, year, director, duration, genre, rate, poster } = req.body
+    
+    if (![title, year, director, duration, genre, rate, poster].every(Boolean))
+        return res.status(400).json({ message: "Datos incompletos." })
+
+    next()
+}
+
+module.exports = validateMovie;
--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 const { getMoviesController, postMovieController } = require('../controllers/moviesController');
+const validateMovie = require('../middlewares/validateMovie');
 
 const router = Router();
 
 router.get('/movies', getMoviesController);
-router.post('/movies', postMovieController);
+router.post('/movies', validateMovie, postMovieController);
 
 
 module.exports = router
